1ae8ad0aad7bdd2ed4c3c4109039b99a643c204f
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-06-19 22:46:54 +0530

    #2

---
+++ Lecture 1/script.js
@@ -0,0 +1,18 @@
+//funndamentals of javascript
+//array and object
+// function return
+// async js coding
+//forEach, map, filter, find, indexOf
+
+// var arr = [1,2,3,4];
+// arr.forEach(function(val){
+
+//     console.log(val+ " hello");
+// })
+
+
+let arr = [1,2,3,4,5];
+let ans = arr.map(function(val){
+    return 13;
+})
+console.log(ans)
